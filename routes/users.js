7588403aac112c44d8ed61fb92ef1f4286c408f8
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-06-10 17:49:18 -0600

    Add routes for users.js with auth and tokens, add routes to server.js

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,18 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const knex = require('../knex');
 const saltRounds = 10;
+const secret = process.env.SECRET;
 
 
-router.get('/users', (req, res, next)=>{
+router.get('/users', (req, res, next) => {
+  // do we need gets for users?
+});
+
+router.get('/users/:id', (req, res, next) => {
 
 });
 
-router.get('/users/:id', (req, res, next)=>{
-
-});
-
-router.post('/users', (req, res, next)=>{
+router.post('/users', (req, res, next) => {
   const body = req.body;
 
   if (!body.first_name) {
@@ -43,13 +44,65 @@ router.post('/users', (req, res, next)=>{
 
   body.password = bcrypt.hashSync(body.password, saltRounds);
 
-  
+  knex('users')
+    .insert(body)
+    .returning([
+      'id',
+      'first_name',
+      'last_name',
+      'email',
+      'username'
+    ])
+    .then((newUser) => {
+      const token = jwt.sign(newUser[0], secret);
+
+      res.cookie('token', token, { httpOnly: true });
+      res.redirect('../public/userpage.html');
+    })
+    .catch((error) => {
+      if (error) {
+        return console.error(error);
+      }
+      res.status(400)
+          .set({ 'Content-Type': 'plain/text' })
+          .send('Email already exists');
+    });
 });
 
-router.patch('/users/:id', (req, res, next)=>{
+router.patch('/users/:id', (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body;
 
+  knex('users')
+    .where('id', id)
+    .update(body)
+    .then(() => {
+      // not sure what to put here
+    })
+    .catch((error) => {
+      if (error) {
+        return console.error(error);
+      }
+      return res.status(404)
+        .set({ 'Content-Type': 'plain/text' })
+        .send('Not Found');
+    });
 });
 
-router.delete('/users/:id', (req, res, next)=>{
-
+router.delete('/users/:id', (req, res, next) => {
+  const id = req.params.id;
+  knex('users')
+    .del()
+    .where('id', id)
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        return res.status(404)
+          .set({ 'Content-Type': 'plain/text' })
+          .send('User not found');
+      }
+      res.redirect('../public/index.html');
+    })
+    .catch((error) => {
+      return res.send(error);
+    });
 });
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,10 +8,17 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 8000;
 
+const notes = require('./routes/notes');
+const users = require('./routes/users');
+const videos = require('./routes/videos');
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
+app.use(notes);
+app.use(users);
+app.use(videos);
 
 app.listen(port, ()=>{
   console.log('Listening on port ' + port);
