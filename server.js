6470fe631b57ce42274e6559615501c8a3524d7c
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-06-09 23:17:14 -0600

    Add parenthesis to app.use

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static('public');
+app.use(express.static('public'));
 
 
 app.listen(port, ()=>{
