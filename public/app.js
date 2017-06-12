190b6e43eda36a52ddd83e9acde626685059f2f0
Sean Eichenberger <sean.eich@gmail.com>
2017-06-12 12:47:29 -0600

    prime search

--- public/app.js
+++ public/app.js
@@ -1,3 +1,27 @@
-$( document ).ready(function() {
-    //console.log( "ready!" );g
-});
+// $( document ).ready(function() {
+//     //console.log( "ready!" );g
+// });
+
+(function (){
+  $('#search').on('click', function(event){
+    console.log(event);
+    event.preventDefault();
+    var request = gapi.client.youtube.search.list({
+      part: "snippit",
+      type: "video",
+      q: encodeURIComponent$(('#search').val()).replace(/%20/g, "+"),
+      maxResults: 3,
+      order: "viewCount",
+    })
+    request.execute(function(response){
+      console.log(response)
+    })
+  })
+})
+
+function init() {
+  gapi.client.setApiKey('AIzaSyC0b4jxH6E1DbtJm3S_ZOZx5ahcOmthPDk');
+  gapi.client.load('youtube', 'v3', function(){
+    //ytapi is ready
+  })
+}
