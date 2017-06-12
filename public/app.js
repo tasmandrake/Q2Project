bc7d61281adb7c36c42e56cb10341c759504c3cf
wallytreats <nicklawrenceemail@gmail.com>
2017-06-12 17:20:33 -0600

    grab video time

--- public/app.js
+++ public/app.js
@@ -2,24 +2,24 @@
 $(document).ready(function() {
 
   var clicked = 0;
-  // var time = timeConvert(player.getCurrentTime());
+  // var time = player.getCurrentTime();
 
-  function timeConvert (time){
-    if (time > 60){
-      minutes = Math.floor(time / 60);
-      seconds = time % 60;
-    }
-    if (seconds < 10){
-      seconds = "0" + seconds;
-    }
-    return minutes + ":" + seconds;
-  }
+  // function timeConvert (time){
+  //   if (time > 60){
+  //     minutes = Math.floor(time / 60);
+  //     seconds = time % 60;
+  //   }
+  //   if (seconds < 10){
+  //     seconds = "0" + seconds;
+  //   }
+  //   return minutes + ":" + seconds;
+  // }
 
   $("#notes").keyup(function() {
     if (clicked === 0){
       // alert("Key press detected!");
       clicked = 1;
-      $("#middle").append(timeConvert(player.getCurrentTime()));
+      $("#middle").append(player.getCurrentTime());
       // $(this).data(time);
       // $('input:textbox').val("new-text-message");
     }else if(clicked > 0){
