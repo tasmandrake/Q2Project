afe8462f85fa5de1a0856fa357e93c3850957408
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-06-12 12:56:58 -0600

    Add styling for modals

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,11 @@
-(function (){
-  $('.touch').click(function(event){
+// globals $, window
+
+'use strict';
+
+(() => {
+  $('#userLogin').click((event) => {
     event.preventDefault();
-    const email= $('#username').val();
+    const email = $('#username').val();
     const password = $('#password').val();
 
     // if(!username){
@@ -13,10 +17,49 @@
 
     const options = {
       contentType: 'application/json',
-      data: JSON.stringify({email: email, password: password}),
+      data: JSON.stringify({ email: email, password: password }),
       dataType: 'json',
       type: 'POST',
       url: '/token'
+    };
+
+    $.ajax(options)
+      .done((data) => {
+        window.location.href = '/login.html';
+      })
+      .fail(($xhr) => {
+        console.log('fail');
+        // Materialize.toast($xhr.responseText, 3000);
+    });
+  });
+
+  $('#newAccount').click((event) => {
+    event.preventDefault();
+    const email = $('#newUsername').val();
+    const password = $('#newPassword').val();
+    const first_name = $('#first_name').val();
+    const last_name = $('#last_name').val();
+    const username = $('#newUsername').val();
+    const data = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      username: username,
+      password: password
+    }
+    // if(!username){
+    //   //return toast
+    // }
+    // if(!password){
+    //   //return toast
+    // }
+
+    const options = {
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      dataType: 'json',
+      type: 'POST',
+      url: '/users'
     }
 
     $.ajax(options)
