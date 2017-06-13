8003db77666466d6a832baff625b39bd99ce2ce8
Sean Eichenberger <sean.eich@gmail.com>
2017-06-13 11:18:08 -0600

    added innerjoin to notes route, dynamic appending myvids, redirect pushed with data

--- seeds/2_videos.js
+++ seeds/2_videos.js
@@ -4,8 +4,8 @@ exports.seed = (knex, Promise) => {
   return knex('videos').del()
     .then(() => {
       return knex('videos').insert([
-        { id: 1, video_url: 'Ct96Y751-_E', img:'https://cdn-images-1.medium.com/max/800/1*jAG7WcT_5ORS6BmpI1LJGg.jpeg', title: 'arrow funcitons', description: 'this video is everything that you need to know about lorem ipsume sti dolorum anum.'},
-        { id: 2, video_url: 'https://www.youtube.com/watch?v=48KJ5HUY6NE', img:'https://cdn-images-1.medium.com/max/800/1*jAG7WcT_5ORS6BmpI1LJGg.jpeg', title:'video title', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit'}
+        { id: 1, video_url: 'Tllw4EPhLiQ&t=5s', img:'https://cdn-images-1.medium.com/max/800/1*jAG7WcT_5ORS6BmpI1LJGg.jpeg', title: 'arrow funcitons', description: 'this video is everything that you need to know about lorem ipsume sti dolorum anum.'},
+        { id: 2, video_url: 'wfMtDGfHWpA', img:'https://cdn-images-1.medium.com/max/800/1*jAG7WcT_5ORS6BmpI1LJGg.jpeg', title:'video title', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit'}
       ]);
     })
     .then(() => {
