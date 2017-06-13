c0a460d5bf094c565e76817ad2ac3c01cc436b32
Sean Eichenberger <sean.eich@gmail.com>
2017-06-13 14:03:42 -0600

    timestamping and notetaking working, but bug with video reload after 5seconds

--- public/app.js
+++ public/app.js
@@ -2,23 +2,13 @@ $(document).ready(function() {
   logout();
 
   var clicked = 0;
-  // var time = player.getCurrentTime();
 
-  // function timeConvert (time){
-  //   if (time > 60){
-  //     minutes = Math.floor(time / 60);
-  //     seconds = time % 60;
-  //   }
-  //   if (seconds < 10){
-  //     seconds = "0" + seconds;
-  //   }
-  //   return minutes + ":" + seconds;
-  // }
 
   $("#notes").keyup(function() {
     if (clicked === 0){
       // alert("Key press detected!");
       clicked = 1;
+      //console.log(player.getCurrentTime());
       $("#middle").append(player.getCurrentTime());
       // $(this).data(time);
       // $('input:textbox').val("new-text-message");
