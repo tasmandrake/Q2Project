282a4ecb825255353622737ef26ba32be1874628
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-06-13 10:24:05 -0600

    Add toasts for client and server side validation

--- public/js/login.js
+++ public/js/login.js
@@ -4,6 +4,94 @@
   hideLogIn();
   hideAcc();
   hideOAuth();
+  login();
+  newUser();
+
+  function login() {
+    $('#userLogin').click((event) => {
+      event.preventDefault();
+      const email = $('#username').val();
+      const password = $('#password').val();
+      const data = {
+        email: email,
+        password: password
+      };
+      const options = {
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        type: 'POST',
+        url: '/token'
+      };
+
+      if (!email) {
+        return toasts('Please enter your email');
+      } else if (!password) {
+        return toasts('Please enter your password');
+      }
+      $.ajax(options)
+        .done((jsonData) => {
+          window.location.href = '/userhub.html';
+        })
+        .fail(($xhr) => {
+          console.log($xhr);
+          return toasts($xhr.responseText);
+        });
+    });
+  }
+
+  function newUser() {
+    $('#newAccount').click((event) => {
+      event.preventDefault();
+      const email = $('#newUsername').val();
+      const password = $('#newPassword').val();
+      const first_name = $('#first_name').val();
+      const last_name = $('#last_name').val();
+      const username = $('#newUsername').val();
+      const data = {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        username: username,
+        password: password
+      };
+      const options = {
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        type: 'POST',
+        url: '/users'
+      };
+
+      if (!email) {
+        return toasts('Please enter your email');
+      } else if (!password) {
+        return toasts('Please enter a password');
+      } else if (!first_name) {
+        return toasts('Please enter a first name');
+      } else if (!last_name) {
+        return toasts('Please enter your last name');
+      } else if (!username) {
+        return toasts('Please enter a username');
+      }
+      $.ajax(options)
+        .done((jsonData) => {
+          window.location.href = '/userhub.html';
+        })
+        .fail(($xhr) => {
+          return toasts($xhr.responseText);
+        })
+    });
+  }
+
+  function toasts(message) {
+    const $snackbar = $('#snackbar');
+    $snackbar.text(message);
+    $snackbar.addClass('show');
+    setTimeout(() => {
+      $snackbar.toggleClass('show');
+    }, 3000);
+  }
 
   function hideLogIn() {
     $('#log').click(() => {
@@ -13,6 +101,7 @@
       $('#info').toggleClass('hidden');
     });
   }
+
   function hideAcc() {
     $('#acc').click(() => {
       $('#info').toggleClass('hidden');
@@ -21,6 +110,7 @@
       $('#info').toggleClass('hidden');
     });
   }
+
   function hideOAuth() {
     $('#oAuth').click(() => {
       $('#info').toggleClass('hidden');
@@ -29,60 +119,4 @@
       $('#info').toggleClass('hidden');
     });
   }
-
-  $('#userLogin').click((event) => {
-    event.preventDefault();
-    const email = $('#username').val();
-    const password = $('#password').val();
-    const data = {
-      email: email,
-      password: password
-    };
-    const options = {
-      contentType: 'application/json',
-      data: JSON.stringify(data),
-      dataType: 'json',
-      type: 'POST',
-      url: '/token'
-    };
-
-    $.ajax(options)
-      .done((jsonData) => {
-        window.location.href = '/userhub.html';
-      })
-      .fail(($xhr) => {
-
-    });
-  });
-
-  $('#newAccount').click((event) => {
-    event.preventDefault();
-    const email = $('#newUsername').val();
-    const password = $('#newPassword').val();
-    const first_name = $('#first_name').val();
-    const last_name = $('#last_name').val();
-    const username = $('#newUsername').val();
-    const data = {
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      username: username,
-      password: password
-    };
-    const options = {
-      contentType: 'application/json',
-      data: JSON.stringify(data),
-      dataType: 'json',
-      type: 'POST',
-      url: '/users'
-    }
-
-    $.ajax(options)
-      .done((jsonData) => {
-        window.location.href = '/userhub.html';
-      })
-      .fail(($xhr) => {
-        console.log($xhr.responseText);
-      })
-  });
 })();
